09c215c0dbc95c4ac9ef856637c3f46b015cfd2a
soni <soni.neosoft@gmail.com>
2022-07-20 14:57:47 +0530

    saas

---
+++ js/custom.js
@@ -0,0 +1,57 @@
+
+$(document).ready(function(){
+  $('.owl-one').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:false,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:1
+          },
+          1000:{
+              items:1
+          }
+      }
+  });
+
+  $('.owl-two').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText: ["<img src='../images/Icon metro-arrow-left.png'>",
+    "<img src='../images/Icon metro-arrow-right.png'>"],
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }      
+      }
+  });
+
+  $('.owl-three').owlCarousel({
+      loop:false,
+      margin:0,
+      nav:false,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:2
+          },
+          1000:{
+              items:3
+          }
+      }
+  });
+});
+
